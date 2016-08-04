02c7c3fc96402fb0a61b38a0ea14eada9f2d8bb5
Nathan Byers <natebyers@gmail.com>
2016-08-03 21:19:20 -0500

    end of day

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ library(plotly)
 library(sunburstR)
 library(leaflet)
 load("data/prepped_data.rda")
-
+hist_color = "springgreen2"
 
 
 ###### User Interface #####################################################3
@@ -14,7 +14,8 @@ ui <- dashboardPage(
   dashboardHeader(title = "TalkingData Explorer"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Map", tabName = "map", icon = icon("globe")),
+      menuItem("Map", tabName = "map", icon = icon("globe"))
+      ,
       menuItem("Phone Brand", tabName = "phone", icon = icon("mobile"))
     )
   ),
@@ -24,55 +25,50 @@ ui <- dashboardPage(
               fluidRow(
                 box(width = 7, 
                     leafletOutput("map", height = 550)
-                    ),
+                    )
+                ,
                 box(width = 4, offset = 1,
                     title = "Demographics (in view)",
                     p("Below are plots of age and gender based on the data for events
                       that are within the boundary of the map."),
                     textOutput("n"),
-                    plotOutput("age_histogram", height = 250),
-                    plotOutput("gender_bar", height = 200)
+                    plotlyOutput("age_histogram", height = 250),
+                    plotlyOutput("gender_bar", height = 200)
                     )
               )
-      ),
-      
+      )
+      ,
+
       tabItem(tabName = "phone",
                      fluidRow(
-                       box(width = 6, sunburstOutput("sunburst", height = 300)),
-                       box(plotlyOutput("brands_bar"), width = 6, height =300,
+                       box(width = 6, sunburstOutput("sunburst", height = 325)),
+                       box(plotlyOutput("brands_bar", height =325), width = 6,
                            title = "Top 25 brands")
                      ),
               fluidRow(
-                
-                box(width = 6, title = "Control Panel",
-                    column(width = 6,
-                           checkboxGroupInput("region", "Select city", 
+
+                box(width = 6, title = "Select a city",
+                    p("Subset the data by selecting one or more cities."),
+                    textOutput("n_phone"),
+                    checkboxGroupInput("region", "City",
                                        choices = c("Beijing", "Chengdu",
                                                    "Hong Kong", "Shanghai"),
                                        selected = c("Beijing", "Chengdu",
-                                                    "Hong Kong", "Shanghai"))
-                      
-                    ),
-                    column(width = 6,
-                           checkboxGroupInput("time_of_day", "Time of the day",
-                                              choices = c("morning", "midday", 
-                                                          "evening", "night"),
-                                              selected = c("morning", "midday", 
-                                                           "evening", "night"))
-                           )
+                                                    "Hong Kong", "Shanghai"),
+                                       inline = TRUE)
                     ),
                 box(width = 6, title = "Demographics",
                     column(width = 6,
-                           plotOutput("phone_age_histogram", height  = 200)
+                           plotlyOutput("phone_age_histogram", height  = 200)
                     ),
                     column(width = 6,
-                           plotOutput("phone_gender_bar", height = 200)
+                           plotlyOutput("phone_gender_bar", height = 200)
                            )
                            )
-                    
+
               )
-                     
-              
+
+
       )
     )
   )
@@ -94,7 +90,7 @@ server <- function(input, output) {
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
       setView(lng = 112.80, lat = 31.85, zoom = 4) %>%
-      addCircleMarkers() %>%
+      addCircleMarkers(radius = map_summary$radius) %>%
       addPopups(map_summary$longitude, map_summary$latitude, overview_content)
   })
   
@@ -112,62 +108,71 @@ server <- function(input, output) {
       select(device_id, gender, age) %>%
       distinct()
   })
-  
+
   output$n <- renderText({
     paste("Total number of people:", dim(eventsInBounds())[1])
   })
 
-
-  output$age_histogram <- renderPlot({
+  
+  output$age_histogram <- renderPlotly({
     
     if (nrow(eventsInBounds()) == 0)
       return(NULL)
-
-    hist(eventsInBounds()$age,
-         main = "",
-         xlab = "Age",
-         col = '#00DD00',
-         border = 'white')
+    
+    plot_ly(x = eventsInBounds()$age, autobinx = F, type = "histogram",
+            xbins = list(start = 0, end = 100, size = 5) ,
+            marker = list(color = toRGB(hist_color))) %>%
+      layout(xaxis = list(title = "Age"))
   })
+
   
-  output$gender_bar <- renderPlot({
-    # If no zipcodes are in view, don't plot
+  output$gender_bar <- renderPlotly({
     if (nrow(eventsInBounds()) == 0)
       return(NULL)
     
     counts <- table(eventsInBounds()$gender)
     
-    barplot(counts, xlab = "Gender", col = 'dodgerblue', border = 'white')
+    p <- plot_ly(
+      x = names(counts),
+      y = counts,
+      name = "Gender",
+      type = "bar",
+      marker = list(color = toRGB("dodgerblue"))) %>%
+      layout(xaxis = list(title = "Gender"))
+    p
   })
 
   ##### Phone Brands ###############################################
   getBrandData <- reactive({
     d <- full_data %>%
-      filter(region %in% input$region, time %in% input$time_of_day) %>%
+      filter(region %in% input$region) %>%
       select(device_id, phone_brand, device_model, gender, age) %>%
       distinct()
-    
+
     d
   })
   
-  
+  output$n_phone <- renderText({
+    paste("Total number of devices:", dim(getBrandData())[1])
+  })
+
+
   output$brands_bar <- renderPlotly({
     d <- getBrandData() %>%
       group_by(phone_brand) %>%
       summarize(total = n()) %>%
       arrange(desc(total)) %>%
       as.data.frame()
-    
-    gg <- ggplot(d[1:25, ], aes(reorder(phone_brand, -total), total,
-                                fill = phone_brand)) + 
-      geom_bar(stat = "identity") + 
+
+    gg <- ggplot(d[1:25, ], aes(phone_brand, total, fill = phone_brand)) +
+      geom_bar(stat = "identity") +
       theme(legend.position = "none", axis.text.x=element_text(angle=90),
             axis.title.x=element_blank())
     # Convert the ggplot to a plotly
     p <- ggplotly(gg)
     p
   })
-  
+
   getPhoneDataSunburst <- reactive({
     d <- getBrandData() %>%
       mutate(brand = gsub(" ", "_", phone_brand),
@@ -175,34 +180,41 @@ server <- function(input, output) {
              sunburst = paste(brand, model, "end", sep = "-")) %>%
       group_by(sunburst) %>%
       summarize(total = n()) %>%
-      arrange(desc(total)) %>% 
+      arrange(desc(total)) %>%
       select(sunburst, total) %>%
       rename(V1 = sunburst, V2 = total) %>%
       as.data.frame()
-    
-    d 
+
+    d
   })
-  
+
   output$sunburst <- renderSunburst({
     d <- getPhoneDataSunburst()
     sunburst(d)
   })
-  
-  output$phone_age_histogram <- renderPlot({
+
+  output$phone_age_histogram <- renderPlotly({
     
-    hist(getBrandData()$age,
-         main = "",
-         xlab = "Age",
-         col = '#00DD00',
-         border = 'white')
+    plot_ly(x = getBrandData()$age, autobinx = F, type = "histogram",
+            xbins = list(start = 0, end = 100, size = 5) ,
+            marker = list(color = toRGB(hist_color))) %>%
+      layout(xaxis = list(title = "Age"))
   })
+
+  output$phone_gender_bar <- renderPlotly({
   
-  output$phone_gender_bar <- renderPlot({
     counts <- table(getBrandData()$gender)
     
-    barplot(counts, xlab = "Gender", col = 'dodgerblue', border = 'white')
+    p <- plot_ly(
+      x = names(counts),
+      y = counts,
+      name = "Gender",
+      type = "bar",
+      marker = list(color = toRGB("dodgerblue"))) %>%
+      layout(xaxis = list(title = "Gender"))
+    p
   })
-    
+
 }
 
 shinyApp(ui, server)
